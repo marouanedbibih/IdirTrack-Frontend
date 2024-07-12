5c7335218da53e4b592593d96a7bd2669f3a6975
BOUFARRA-OUSSAMA <oussama.boufarra@emsi-edu.ma>
2024-07-12 20:06:58 +0100

    commiting everything

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SimTable from './components/SimTable';
+
+const Home: React.FC = () => {
+    return (
+        <div className="container mx-auto p-4">
+            <SimTable />
+        </div>
+    );
+};
+
+export default Home;
