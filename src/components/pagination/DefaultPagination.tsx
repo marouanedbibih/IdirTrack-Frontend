cefe39a4ea613c2ed1065e67b6fbfb01d38220bf
Marouane Dbibih <marouane.dbibih@outlook.com>
2024-08-08 12:38:07 +0100

    implement the APIs andpoints for get vehicle infos and boities list by vehicle id

--- src/components/pagination/DefaultPagination.tsx
+++ src/components/pagination/DefaultPagination.tsx
@@ -1,55 +1,93 @@
-import React from "react";
-import { Button, IconButton } from "@material-tailwind/react";
-import { ArrowRightIcon, ArrowLeftIcon } from "@heroicons/react/24/outline";
- 
-export function DefaultPagination() {
-  const [active, setActive] = React.useState(1);
- 
-  const getItemProps = (index) =>
-    ({
-      variant: active === index ? "filled" : "text",
-      color: "gray",
-      onClick: () => setActive(index),
-    } as any);
- 
-  const next = () => {
-    if (active === 5) return;
- 
-    setActive(active + 1);
-  };
- 
-  const prev = () => {
-    if (active === 1) return;
- 
-    setActive(active - 1);
-  };
- 
-  return (
-    <div className="flex items-center gap-4">
-      <Button
-        variant="text"
-        className="flex items-center gap-2"
-        onClick={prev}
-        disabled={active === 1}
-      >
-        <ArrowLeftIcon strokeWidth={2} className="h-4 w-4" /> Previous
-      </Button>
-      <div className="flex items-center gap-2">
-        <IconButton {...getItemProps(1)}>1</IconButton>
-        <IconButton {...getItemProps(2)}>2</IconButton>
-        <IconButton {...getItemProps(3)}>3</IconButton>
-        <IconButton {...getItemProps(4)}>4</IconButton>
-        <IconButton {...getItemProps(5)}>5</IconButton>
-      </div>
-      <Button
-        variant="text"
-        className="flex items-center gap-2"
-        onClick={next}
-        disabled={active === 5}
-      >
-        Next
-        <ArrowRightIcon strokeWidth={2} className="h-4 w-4" />
-      </Button>
-    </div>
-  );
+import * as React from "react";
+import { Button, CardFooter, IconButton } from "@material-tailwind/react";
+
+export interface IDefaultPaginationProps {
+  currentPage: number;
+  totalPages: number;
+  onPageChange: (page: number) => void;
 }
+
+const DefaultPagination: React.FC<IDefaultPaginationProps> = ({
+  currentPage,
+  totalPages,
+  onPageChange,
+}) => {
+  const getPageNumbers = () => {
+    const range = 10;
+    const halfRange = Math.floor(range / 2);
+    let start = Math.max(1, currentPage - halfRange);
+    let end = Math.min(totalPages, currentPage + halfRange);
+
+    if (currentPage - halfRange <= 0) {
+      end = Math.min(totalPages, end + (halfRange - currentPage + 1));
+    }
+
+    if (currentPage + halfRange >= totalPages) {
+      start = Math.max(1, start - (currentPage + halfRange - totalPages));
+    }
+
+    const pages = [];
+    for (let i = start; i <= end; i++) {
+      pages.push(i);
+    }
+
+    return pages;
+  };
+
+  const handlePageChange = (page: number) => {
+    if (page >= 1 && page <= totalPages) {
+      onPageChange(page);
+    }
+  };
+
+  return (
+    <CardFooter
+      className="flex items-center justify-between border-t border-blue-gray-50 p-4"
+      placeholder={undefined}
+      onPointerEnterCapture={undefined}
+      onPointerLeaveCapture={undefined}
+    >
+      <div className="w-1/2 flex flex-1 gap-8 justify-center">
+        <Button
+          variant="outlined"
+          size="sm"
+          onClick={() => handlePageChange(currentPage - 1)}
+          disabled={currentPage === 1}
+          placeholder={undefined}
+          onPointerEnterCapture={undefined}
+          onPointerLeaveCapture={undefined}
+        >
+          Previous
+        </Button>
+        <div className="flex items-center gap-2">
+          {getPageNumbers().map((page) => (
+            <IconButton
+              key={page}
+              variant={page === currentPage ? "outlined" : "text"}
+              size="sm"
+              onClick={() => handlePageChange(page)}
+              placeholder={undefined}
+              onPointerEnterCapture={undefined}
+              onPointerLeaveCapture={undefined}
+            >
+              {page}
+            </IconButton>
+          ))}
+        </div>
+        <Button
+          variant="outlined"
+          size="sm"
+          onClick={() => handlePageChange(currentPage + 1)}
+          disabled={currentPage === totalPages}
+          placeholder={undefined}
+          onPointerEnterCapture={undefined}
+          onPointerLeaveCapture={undefined}
+        >
+          Next
+        </Button>
+      </div>
+    </CardFooter>
+  );
+};
+
+export default DefaultPagination;
--- src/vehicle/components/VehicleDetailsHeader.tsx
+++ src/vehicle/components/VehicleDetailsHeader.tsx
@@ -1,59 +1,105 @@
 import React from "react";
-import {
-    Button,
-    Card,
-    CardBody,
-    CardFooter,
-    Typography,
-  } from "@material-tailwind/react";
-import { VehicleInterface } from "../VehicleTypes";
-import { Vehicle } from "@/types/Vehicle";
-import { Client } from "@/types/Client";
+import { Card, CardBody, Spinner, Typography } from "@material-tailwind/react";
 
+import { useVehicleContext } from "../contexts/VehicleProvider";
+import { getVehicleByIdAPI } from "../services/vehicleService";
 
-interface VehicleDetailsHeaderProps {
-    vehicleInfos : Vehicle;
-    clientInfos : Client;
-}
+interface VehicleDetailsHeaderProps {}
 
-function VehicleDetailsHeader({ vehicleInfos,clientInfos}: VehicleDetailsHeaderProps) {
+function VehicleDetailsHeader({}: VehicleDetailsHeaderProps) {
+  // Vehicle details provider list
+  const { vehicleDetails, setVehicleDetails } = useVehicleContext();
+
+  // Vehicle ID provider state
+  const { vehicleId, setVehicleId } = useVehicleContext();
+
+  // Loading vehicle details local state
+  const [loading, setLoading] = React.useState<boolean>(false);
+
+  /**
+   * FETCH VEHICLE BY ID
+   *
+   * @param id
+   */
+  const fetchVehicleById = async (id: number) => {
+    setLoading(true);
+    getVehicleByIdAPI(id)
+      .then((response) => {
+        setVehicleDetails(response.content);
+        console.log("Vehicle Details Response", response.content);
+      })
+      .catch((error) => {
+        console.error("Error fetching vehicle by id:", error);
+      })
+      .finally(() => {
+        console.log("Vehicle fetched successfully");
+        setLoading(false);
+      });
+  };
+
+  // useEffect to fetch vehicle by id
+  React.useEffect(() => {
+    if (vehicleId) {
+      fetchVehicleById(vehicleId);
+    }
+    console.log("Vehicle Details", vehicleDetails);
+  }, [vehicleId]);
   return (
     <Card
-      className="mt-6 w-96"
+      className="w-96 min-h-24"
       placeholder={undefined}
       onPointerEnterCapture={undefined}
       onPointerLeaveCapture={undefined}
     >
-      <CardBody
-        placeholder={undefined}
-        onPointerEnterCapture={undefined}
-        onPointerLeaveCapture={undefined}
-      >
-        <Typography
-          variant="h5"
-          color="blue-gray"
-          className="mb-2"
+      {loading ? (
+        <div className="flex flex-1 justify-center items-center">
+          <Spinner
+            onPointerEnterCapture={undefined}
+            onPointerLeaveCapture={undefined}
+          />
+        </div>
+      ) : (
+        <CardBody
           placeholder={undefined}
           onPointerEnterCapture={undefined}
           onPointerLeaveCapture={undefined}
         >
-          {vehicleInfos?.matricule}
-        </Typography>
-        <Typography
-          placeholder={undefined}
-          onPointerEnterCapture={undefined}
-          onPointerLeaveCapture={undefined}
-        >
-          {clientInfos?.name}
-        </Typography>
-        <Typography
-          placeholder={undefined}
-          onPointerEnterCapture={undefined}
-          onPointerLeaveCapture={undefined}
-        >
-          {clientInfos?.company}
-        </Typography>
-      </CardBody>
+          <Typography
+            variant="h5"
+            color="blue-gray"
+            className="mb-2"
+            placeholder={undefined}
+            onPointerEnterCapture={undefined}
+            onPointerLeaveCapture={undefined}
+          >
+            {vehicleDetails?.vehicle.matricule}
+          </Typography>
+          <Typography
+            placeholder={undefined}
+            onPointerEnterCapture={undefined}
+            onPointerLeaveCapture={undefined}
+          >
+            {vehicleDetails?.vehicle.type}
+          </Typography>
+          <Typography
+            variant="h5"
+            color="blue-gray"
+            className="mb-2"
+            placeholder={undefined}
+            onPointerEnterCapture={undefined}
+            onPointerLeaveCapture={undefined}
+          >
+            {vehicleDetails?.client.name}
+          </Typography>
+          <Typography
+            placeholder={undefined}
+            onPointerEnterCapture={undefined}
+            onPointerLeaveCapture={undefined}
+          >
+            {vehicleDetails?.client.company}
+          </Typography>
+        </CardBody>
+      )}
     </Card>
   );
 }
